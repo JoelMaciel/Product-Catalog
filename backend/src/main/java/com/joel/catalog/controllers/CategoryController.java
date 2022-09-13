0aa25e1aca1a92706ae9a00c94c3397d985efa7a
Joel Maciel <soldado25461@gmail.com>
2022-09-13 19:17:04 -0300

    Pagination

--- backend/src/main/java/com/joel/catalog/controllers/CategoryController.java
+++ backend/src/main/java/com/joel/catalog/controllers/CategoryController.java
@@ -1,9 +1,11 @@
 package com.joel.catalog.controllers;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,9 +30,17 @@ public class CategoryController {
 	private CategoryService categoryService;
 	
 	@GetMapping
-	public ResponseEntity<List<CategoryDto>> findAll(){
-		List<CategoryDto> list = categoryService.findAll();
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<Page<CategoryDto>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction
+			){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		Page<CategoryDto> pages = categoryService.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(pages);
 	}
 	
 	@GetMapping("/{categoryId}")
