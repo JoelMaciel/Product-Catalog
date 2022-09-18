755a63f5c834bd2f1912d559e979fb3b9c7e8589
Joel Maciel <soldado25461@gmail.com>
2022-09-18 19:19:51 -0300

    Finalzando testes da ProductController, camada Web

--- backend/src/test/java/com/joel/catalog/controllers/ProductControllerTests.java
+++ backend/src/test/java/com/joel/catalog/controllers/ProductControllerTests.java
@@ -2,8 +2,12 @@ package com.joel.catalog.controllers;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,6 +27,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.joel.catalog.dto.ProductDto;
 import com.joel.catalog.services.ProductService;
+import com.joel.catalog.services.exception.DatabaseException;
 import com.joel.catalog.services.exception.ResourceNotFoundException;
 import com.joel.catalog.tests.Factory;
 
@@ -40,6 +45,7 @@ public class ProductControllerTests {
 	
 	private long existingId;
 	private long nonExistingId;
+	private long depedentId;
 	private ProductDto productDto;
 	private PageImpl<ProductDto> page;
 	
@@ -48,6 +54,7 @@ public class ProductControllerTests {
 		
 		existingId = 1L;
 		nonExistingId = 2L;
+		depedentId = 3L;
 		
 		productDto = Factory.createProductDto();
 		page = new PageImpl<>(List.of(productDto));
@@ -56,8 +63,51 @@ public class ProductControllerTests {
 		when(productService.findById(existingId)).thenReturn(productDto);
 		when(productService.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
 		
+		when(productService.save(any())).thenReturn(productDto);
+		
 		when(productService.update(eq(existingId), any())).thenReturn(productDto);
 		when(productService.update(eq(nonExistingId), any())).thenThrow(ResourceNotFoundException.class);
+		
+		doNothing().when(productService).delete(existingId);
+		doThrow(ResourceNotFoundException.class).when(productService).delete(nonExistingId);
+		doThrow(DatabaseException.class).when(productService).delete(depedentId);
+		
+		
+	}
+	
+	@Test
+	public void deleteShouldReturnNoContentWhenIdExists() throws Exception {
+		ResultActions result = mockMvc.perform(delete("/products/{id}", existingId)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void deleteShouldReturnNotFoundtWhenIdDoesNotExists() throws Exception {
+		
+		ResultActions result = mockMvc.perform(delete("/products/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());
+		
+		
+	}
+	
+	@Test
+	public void insertShouldReturnProductDtoCreated() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDto);
+		
+		ResultActions result = mockMvc.perform(post("/products")
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
 		
 	}
 	
