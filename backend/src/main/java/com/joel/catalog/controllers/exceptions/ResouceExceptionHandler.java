fda9933fb54d6850a1e1ceeba9592e5ae9b12a30
Joel Maciel <soldado25461@gmail.com>
2022-09-21 09:45:35 -0300

    Custom validation response

--- backend/src/main/java/com/joel/catalog/controllers/exceptions/ResouceExceptionHandler.java
+++ backend/src/main/java/com/joel/catalog/controllers/exceptions/ResouceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -40,7 +42,25 @@ public class ResouceExceptionHandler {
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> database(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		
+		var error = new ValidationError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Validation exception !");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		for(FieldError fielError : e.getBindingResult().getFieldErrors()) {
+			error.addError(fielError.getField(), fielError.getDefaultMessage());
+		};
+		
+		return ResponseEntity.status(status).body(error);
 	}
 
 }
