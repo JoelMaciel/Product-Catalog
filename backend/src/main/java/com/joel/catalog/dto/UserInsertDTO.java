97c18dc626e905cf1d60d80feb17ced29f2f0641
Joel Maciel <soldado25461@gmail.com>
2022-09-21 10:13:51 -0300

    Custom contraint validation

--- backend/src/main/java/com/joel/catalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/joel/catalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.joel.catalog.dto;
 
+import com.joel.catalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
 	private String password;
---
+++ backend/src/main/java/com/joel/catalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,40 @@
+package com.joel.catalog.services.validation;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.joel.catalog.controllers.exceptions.FieldMessage;
+import com.joel.catalog.dto.UserInsertDTO;
+import com.joel.catalog.repositories.UserRepository;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void initialize(UserInsertValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		var user = userRepository.findByEmail(dto.getEmail());
+		if(user != null) {
+			list.add(new FieldMessage("email", "Email ja existe"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
